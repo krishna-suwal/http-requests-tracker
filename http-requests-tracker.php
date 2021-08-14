5380b77046487a4d46fcdbb3e07a85364e62dd1d
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2021-08-14 13:50:07 +0545

    Setup phpcs

--- http-requests-tracker.php
+++ http-requests-tracker.php
@@ -12,31 +12,31 @@
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
-define('HRT_SLUG', 'hrt');
-define('HRT_VERSION', '0.1.0');
-define('HRT_PLUGIN_FILE', __FILE__);
-define('HRT_PLUGIN_BASENAME', plugin_basename(__FILE__));
-define('HRT_PLUGIN_DIR', dirname(__FILE__));
-define('HRT_ASSETS', dirname(__FILE__) . '/assets');
-define('HRT_TEMPLATES', dirname(__FILE__) . '/templates');
-define('HRT_LANGUAGES', dirname(__FILE__) . '/i18n/languages');
-define('HRT_PLUGIN_REL_LANGUAGES_PATH', 'i18n/languages');
+define( 'HRT_SLUG', 'hrt' );
+define( 'HRT_VERSION', '0.1.0' );
+define( 'HRT_PLUGIN_FILE', __FILE__ );
+define( 'HRT_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+define( 'HRT_PLUGIN_DIR', dirname( __FILE__ ) );
+define( 'HRT_ASSETS', dirname( __FILE__ ) . '/assets' );
+define( 'HRT_TEMPLATES', dirname( __FILE__ ) . '/templates' );
+define( 'HRT_LANGUAGES', dirname( __FILE__ ) . '/i18n/languages' );
+define( 'HRT_PLUGIN_REL_LANGUAGES_PATH', 'i18n/languages' );
 
 /**
  * Include the autoloader.
  *
  * @since 0.1.0
  */
-require_once dirname(__FILE__) . '/vendor/autoload.php';
+require_once dirname( __FILE__ ) . '/vendor/autoload.php';
 
 /**
  * Bootstrap the appplication.
  *
  * @since 0.1.0
  */
-require_once dirname(__FILE__) . '/bootstrap/app.php';
+require_once dirname( __FILE__ ) . '/bootstrap/app.php';
 
 /**
  * Return the service provider.
@@ -44,12 +44,11 @@ require_once dirname(__FILE__) . '/bootstrap/app.php';
  * @since 0.1.0
  *
  * @param string $class Class name or alias.
- * 
+ *
  * @return HRT\HTTPRequestsTracker|mixed
  */
-function hrt( $class = 'app' )
-{
-    return empty($class) ? $GLOBALS['hrt'] : $GLOBALS['hrt']->get($class);
+function hrt( $class = 'app' ) {
+	return empty( $class ) ? $GLOBALS['hrt'] : $GLOBALS['hrt']->get( $class );
 }
 
 hrt();
