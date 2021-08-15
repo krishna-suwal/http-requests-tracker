06c8238db2a5e67fa7be69f791ce94a07691045a
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2021-08-15 11:26:17 +0545

    Update app bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,33 +1,32 @@
 <?php
 /**
- * Initialize the application.
+ * Bootstrap the application.
  *
  * @since 0.1.0
  */
 
-function hrt_init_app()
-{
-    global $hrt;
+function hrt_boot_app() {
+	global $hrt;
 
-    if ($hrt ) {
-        return $hrt;
-    }
-    $hrt = new League\Container\Container();
-    
-    /**
-     * Enable the auto wiring.
-     */
-    $hrt->delegate(
-        new League\Container\ReflectionContainer()
-    );
-    
-    $hrt_service_providers = include_once __DIR__ . '/providers.php';
-    
-    foreach ( $hrt_service_providers as $p ) {
-        $hrt->addServiceProvider($p);
-    }
+	if ( $hrt ) {
+		return $hrt;
+	}
+	$hrt = new League\Container\Container();
 
-    return $hrt;
+	/**
+	 * Enable the auto wiring.
+	 */
+	$hrt->delegate(
+		new League\Container\ReflectionContainer()
+	);
+
+	$hrt_service_providers = include_once __DIR__ . '/providers.php';
+
+	foreach ( $hrt_service_providers as $p ) {
+		$hrt->addServiceProvider( $p );
+	}
+
+	return $hrt;
 }
 
-return hrt_init_app();
+return hrt_boot_app();
