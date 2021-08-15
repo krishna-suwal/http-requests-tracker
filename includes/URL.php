6e1fef0bb1c5043975bdd129c26158b78a7b449e
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2021-08-15 09:56:34 +0545

    Created class: URL

---
+++ includes/URL.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * URL class.
+ *
+ * @package HRT
+ *
+ * @since 0.1.0
+ */
+
+namespace HRT;
+
+use Wa72\Url\Url as UrlLib;
+
+defined( 'ABSPATH' ) || exit;
+
+class URL extends UrlLib {
+}
